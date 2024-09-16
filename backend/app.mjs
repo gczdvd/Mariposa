81d451df5ebfd86e043d91f32a53d3bbd714828e
gcz.dvd <gogodavid19@gmail.com>
2024-09-16 13:01:33 +0200

    MySQL Test

--- backend/app.mjs
+++ backend/app.mjs
@@ -3,6 +3,24 @@
 import { WebSocketServer } from "ws";
 import { User } from "./User.mjs";
 
+import mysql from "mysql";
+
+try{
+    var con = mysql.createConnection({
+        host: "172.16.193.50",
+        user: "root",
+        password: "root"
+    });
+
+    con.connect((err)=>{
+        console.log("Connected!");
+    });
+}
+catch(e){
+    console.log(e);
+}
+
+/*
 var u = new User(3);
 console.log(u.ageszor());
 
@@ -10,4 +28,4 @@ const WebSoc = new WebSocketServer({port:8081});
 
 WebSoc.addListener("connection", (e)=>{
     console.log("Csatlakoztak");
-});
+});*/
