866b07792423d21241483fbf716648acb2b51392
rXenia <xrbnxx@gmail.com>
2024-10-14 15:38:49 +0200

    Deleted old folders and copied new folders into frontend folder. Was studying grid, started working on the new files in a different folder, and now uploaded it to the repo.

---
+++ frontend/landing page/script.js
@@ -0,0 +1,14 @@
+let slideIndex = ;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "grid";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
