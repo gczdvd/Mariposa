20960aacfbb3da1b01d03e3544213b826d1f7eb7
rXenia <xrbnxx@gmail.com>
2024-11-13 19:59:20 +0100

    .

--- frontend/support/script.js
+++ frontend/support/script.js
@@ -18,9 +18,47 @@ for (i = 0; i < acc.length; i++) {
 
 function myFunction() {
   var x = document.getElementById("navigation");
+  var logo = document.getElementById("logo");
+  
+
   if (x.style.display === "block") {
+    
     x.style.display = "none";
+    
   } else {
+
     x.style.display = "block";
+    
+  }
+}
+
+
+const textarea = document.getElementById("textarea");
+function charCounter(inputField) {
+
+  const remChars = document.getElementById("remaining-chars");
+  remChars.style.visibility = "visible";
+  const maxLength = inputField.getAttribute("maxlength");
+  const currentLength = inputField.value.length;
+  remChars.innerHTML = `${maxLength - currentLength} karakter maradt`;
+  
+}
+textarea.oninput = () => charCounter(textarea);
+// textarea.onfocus = () => charCounter(textarea);
+
+textarea.onfocus = () => szinValtoztat(textarea);
+textarea.onfocusout = () => szinValtoztat;
+
+function szinValtoztat(textarea){
+  const remChars = document.getElementById("remaining-chars");
+  // const maxLength = textarea.getAttribute("maxlength");
+
+  if(textarea.value.length == 0){
+    textarea.style.borderColor = "#d3d3d3";
+    remChars.style.visibility = "hidden";
+  }
+  else{
+    textarea.style.borderColor = "#ffbc2f";
+    remChars.style.visibility = "visible";
   }
 }
