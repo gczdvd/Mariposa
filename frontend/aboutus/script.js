055ccfc1055cc794043dd5969e10beff3cc9d688
rXenia <xrbnxx@gmail.com>
2024-11-28 19:29:37 +0100

    .

--- frontend/aboutus/script.js
+++ frontend/aboutus/script.js
@@ -1,39 +1,3 @@
-// var acc = document.getElementsByClassName("accordion");
-// var i;
-
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function() {
-//     /* Toggle between adding and removing the "active" class,
-//     to highlight the button that controls the panel */
-//     this.classList.toggle("active");
-
-//     /* Toggle between hiding and showing the active panel */
-//     var panel = this.nextElementSibling;
-//     if (panel.style.display === "block") {
-//       panel.style.display = "none";
-//     } else {
-//       panel.style.display = "block";
-//     }
-//   });
-// }
-
-// Animált
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
-
-
 // Textarea karakterlimit jelző
 const textarea = document.getElementById("textarea");
 
@@ -53,3 +17,5 @@ function charCounter(inputField) {
 }
 
 textarea.onfocus = () => charCounter(textarea);
+
+
