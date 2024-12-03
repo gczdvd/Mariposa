e721914f3e80f568006ee0142bc6f0aea7cc39b2
rXenia <xrbnxx@gmail.com>
2024-12-03 20:37:23 +0100

    .

--- frontend/Chat/script.js
+++ frontend/Chat/script.js
@@ -34,15 +34,29 @@ function placeholderAdd(textarea){
 }
 
 
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-  document.getElementById("sidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("navigation").style.width = "26vw";
+  // document.getElementById("main").style.marginRight = "250px";
+
+  document.getElementById("close").style.display = "block";
+  document.getElementById("open").style.display = "none";
+
+  document.getElementById("wrapper").style.filter = "blur(1)";
+
+  // document.getElementById("header").style.backgroundColor = "orange";
+  // header.style.backgroundColor = "orange";
+
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
-  document.getElementById("sidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
-  
+
+  document.getElementById("navigation").style.width = "0px";
+  // document.getElementById("main").style.marginRight = "0";
+  // document.getElementById("header").style.backgroundColor = "transparent";
+  document.getElementById("header").style.height = "fit-content";
+
+  document.getElementById("open").style.display = "block";
+
+  document.getElementById("close").style.display = "none";
 }
