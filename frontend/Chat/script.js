5b0ad20e7fe8f81319526e733cac537a2a1570fa
rXenia <xrbnxx@gmail.com>
2024-12-05 13:40:36 +0100

    .

--- frontend/Chat/script.js
+++ frontend/Chat/script.js
@@ -1,3 +1,5 @@
+
+
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
     var x = document.getElementById("myLinks");
@@ -41,7 +43,8 @@ function openNav() {
   document.getElementById("close").style.display = "block";
   document.getElementById("open").style.display = "none";
 
-  document.getElementById("wrapper").style.filter = "blur(1)";
+  document.getElementById("blurred").style.filter = "blur(2px)";
+  // document.getElementById("navigation").style.filter = "blur(0px)";
 
   // document.getElementById("header").style.backgroundColor = "orange";
   // header.style.backgroundColor = "orange";
@@ -59,4 +62,55 @@ function closeNav() {
   document.getElementById("open").style.display = "block";
 
   document.getElementById("close").style.display = "none";
+
+  document.getElementById("blurred").style.filter = "blur(0px)";
 }
+
+function savePartner(){
+  Swal.fire({
+    icon: "question",
+    title: "Biztosan menteni szeretnéd partnered?",
+    text: "A mentési szándékról partnered értesítést kap, és elutasíthatja azt!",
+    width: "64em",
+    showCancelButton: "true",
+    reverseButtons: "true",
+    focusConfirm: "false",
+    confirmButtonText: "Mentés", 
+    cancelButtonText: "Mégse",
+    confirmButtonColor: "#ffbc2f"
+  })
+}
+
+
+// Swal.fire({
+//   title: "Do you want to save the changes?",
+//   showDenyButton: true,
+//   showCancelButton: true,
+//   confirmButtonText: "Save",
+//   denyButtonText: `Don't save`
+// }).then((result) => {
+//   /* Read more about isConfirmed, isDenied below */
+//   if (result.isConfirmed) {
+//     Swal.fire("Saved!", "", "success");
+//   } else if (result.isDenied) {
+//     Swal.fire("Changes are not saved", "", "info");
+//   }
+// });
+
+function reportPartner(){
+  Swal.fire({
+    icon: "warning",
+    title: "Biztosan jelenteni szeretnéd partnered?",
+    text: "A jelentés nem vonható vissza. A beszélgetés átvizsgálásához üzeneteid mentésre kerülnek.",
+    width: "64em",
+    showCancelButton: "true",
+    reverseButtons: "true",
+    focusConfirm: "false",
+    confirmButtonText: "Jelentés", 
+    cancelButtonText: "Mégse",
+    confirmButtonColor: "#ffbc2f",
+  })
+}
+
+// Kulcs: html, érték: egész html oldal
+// Repülőékezettel lehet sort törni
