cb3dffd9761692ae7f13fe1adc45f8b9d33793b0
rXenia <xrbnxx@gmail.com>
2025-01-19 13:13:34 +0100

    .

--- test/app.mjs
+++ test/app.mjs
@@ -1,8 +1,8 @@
 import fs from 'fs';
-import nodemailer from 'file://C:/Users/David/AppData/Roaming/npm/node_modules/nodemailer/lib/nodemailer.js';
+import nodemailer from 'file://C:\\Users\\xcsiz\\AppData\\Roaming\\npm\\node_modules\\nodemailer\\lib\\nodemailer.js';
 
 var transport = nodemailer.createTransport({
-    host: "172.30.0.100",
+    host: "172.29.0.100",
     port: 25,
     tls: {
         rejectUnauthorized: false
@@ -11,7 +11,7 @@ var transport = nodemailer.createTransport({
 
 transport.sendMail({
     from: `"Mariposa - The Tester Butterfly" <test@mariposachat.hu>`,
-    to: process.argv[2],
-    subject: process.argv[3],
-    html: fs.readFileSync(process.argv[4], {encoding: 'utf8', flag: 'r'})
+    to: "support@mariposachat.hu",
+    subject: process.argv[2],
+    html: fs.readFileSync(process.argv[3], {encoding: 'utf8', flag: 'r'})
 });
