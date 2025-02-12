bb0008a1c631314d7f182b5572a3008732863c7b
rXenia <xrbnxx@gmail.com>
2025-02-12 20:08:42 +0100

    Added opennav and closenav, finished login auto test, added gap to footer on LANDING PAGE

--- testing/cypress/e2e/login.cy.js
+++ testing/cypress/e2e/login.cy.js
@@ -1,12 +1,58 @@
-describe('template spec', () => {
-  it('login', () => {
+describe('login email fail spec', () => {
+  it('should fail, email with no @ or .com at end', () => {
     cy.visit('https://mariposachat.hu/login')
 
-    cy.get('#email').type('csizmadiaxenia@gmail.com');
-    // cy.get('#password').type('BarackoskerT');
+    cy.get('#email').type('csizmadiaxeniagmail')
+    cy.get('#password').type('UserPassword')
 
-    // cy.get('.primaryBTN').click()
+    cy.get('button[class=primaryBTN]').click()
+    cy.get('#emailFeedback').should('be.visible')
 
-    // cy.get('#chatArea').should(be.visible)
+    cy.wait(2000)
+  })
+})
+
+describe('login password fail spec', () => {
+  it('should fail, password with no capital chars', () => {
+    cy.visit('https://mariposachat.hu/login')
+
+    cy.get('#email').type('csizmadiaxenia@gmail.com')
+    cy.get('#password').type('userpassword')
+
+    cy.get('button[class=primaryBTN]').click()
+    cy.get('#pswFeedback').should('be.visible')
+
+    cy.wait(2000)
+  })
+})
+
+describe('login fail spec', () => {
+  it('should fail, incorrect email and password', () => {
+    cy.visit('https://mariposachat.hu/login')
+
+    cy.get('#email').type('csizmadiaxeniagmail')
+    cy.get('#password').type('userpassword')
+
+    cy.get('button[class=primaryBTN]').click()
+
+    cy.get('#emailFeedback').should('be.visible')
+    cy.get('#pswFeedback').should('be.visible')
+
+    cy.wait(2000)
+  })
+})
+
+describe('login spec', () => {
+  it('should be able to login', () => {
+    cy.visit('https://mariposachat.hu/login')
+
+    cy.get('#email').type('csizmadiaxenia@gmail.com')
+    cy.get('#password').type('UserPassword')
+
+    cy.get('button[class=primaryBTN]').click()
+
+    cy.url().should('include', '/chat')
+    
+    cy.wait(2000)
   })
 })
