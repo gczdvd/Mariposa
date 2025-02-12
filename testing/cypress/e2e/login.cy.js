d6a23919cf710dcea69d73008713914db4361f31
rXenia <xrbnxx@gmail.com>
2025-02-12 11:35:58 +0100

    Added fallback fonts, re-designed CHATAREA to be responsive

--- testing/cypress/e2e/login.cy.js
+++ testing/cypress/e2e/login.cy.js
@@ -1,5 +1,12 @@
 describe('template spec', () => {
   it('login', () => {
-    cy.visit('https://mariposachat.hu')
+    cy.visit('https://mariposachat.hu/login')
+
+    cy.get('#email').type('csizmadiaxenia@gmail.com');
+    // cy.get('#password').type('BarackoskerT');
+
+    // cy.get('.primaryBTN').click()
+
+    // cy.get('#chatArea').should(be.visible)
   })
 })
