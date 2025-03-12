2186ba4d2768f98f8aa434b9f11244c89a7e8e84
rXenia <xrbnxx@gmail.com>
2025-03-12 18:44:41 +0100

    .

--- frontend/registration/script.js
+++ frontend/registration/script.js
@@ -1,17 +1,5 @@
 Backend.setUrl("127.0.0.1:3000");
 
-function togglePSW() {
-    var x = document.getElementById("password");
-    if (x.type === "password") {
-      x.type = "text";
-      document.getElementById("toggleIcon").innerHTML = "👁️";
-      
-    } else {
-      x.type = "password";
-      document.getElementById("toggleIcon").innerHTML = "🕳️";
-    }
-}
-
 function colorChange(textarea){  
     if(textarea.value.length == 0){
       // textarea.style.borderColor = "#d3d3d3";
